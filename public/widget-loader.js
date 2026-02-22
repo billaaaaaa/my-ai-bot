d1f0a3ce8f594bd1e411522b60e2996d2a152059
bilaaaaaa <asifsindhu8074@gmail.com>
2026-02-21 18:22:09 -0800

    Final URL verification for mobile testing

--- public/widget-loader.js
+++ public/widget-loader.js
@@ -1,6 +1,6 @@
 (function () {
     // Configuration
-    const baseUrl = 'http://localhost:3001';
+    const baseUrl = 'https://my-ai-bot-yoje.onrender.com';
 
     // Inject Stylesheet
     const link = document.createElement('link');
